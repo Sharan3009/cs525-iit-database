25b8ca7aa908490c16923b64ac0316585149782e
ssingh <s.singh@gtisoft.com>
2023-09-18 17:44:15 -0500

    updated readPageDirectory algo

--- assign1/page_directory.c
+++ assign1/page_directory.c
@@ -6,10 +6,18 @@
 RC readPageDirectory(SM_FileHandle *fHandle, SM_PageHandle directory){
     if(fHandle==NULL || directory==NULL)
         return RC_FILE_HANDLE_NOT_INIT;
-    for(int i=0;i<PAGE_SIZE;i++){
-        directory[i] = fgetc(fHandle->mgmtInfo);
+    RC ret;
+    int pos = ftell(fHandle->mgmtInfo);
+    if(fseek(fHandle->mgmtInfo, 0, SEEK_SET)==0){
+        for(int i=0;i<PAGE_SIZE;i++){
+            directory[i] = fgetc(fHandle->mgmtInfo);
+        }
+        ret = RC_OK;
+    } else {
+        ret = RC_READ_NON_EXISTING_PAGE;
     }
-    return RC_OK;
+    fseek(fHandle->mgmtInfo, pos, SEEK_SET);
+    return ret;
 }
 
 RC updatePageDirectory(SM_FileHandle *fHandle, SM_PageHandle directory){
